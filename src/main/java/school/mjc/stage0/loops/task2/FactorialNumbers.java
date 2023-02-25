59a8ef0f9f0d5cac3df92508fbe5f0a0fa831374
Mykhailo Derecha <don.miggel@yahoo.com>
2023-02-25 19:01:52 +0200

    Module 4, task done

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -3,5 +3,16 @@ package school.mjc.stage0.loops.task2;
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
 
+        int counter = 1;
+        int res = 1;
+        System.out.println(res);
+        while(counter <= printToInclusive){
+            res *= counter;
+            System.out.println(res);
+            counter++;
+
+        }
+
+
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+
+        int outer = 2;
+        while(outer <= printToInclusive){
+            int inner = 1;
+            int counter = 0;
+            while(inner <= outer){
+                if (outer % inner == 0)
+                    counter++;
+                inner++;
+            }
+            if (counter == 2)
+                System.out.println(outer);
+            outer++;
+        }
     }
 }
